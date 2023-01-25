5bb69af63308ec495bc08d1d6df8fe02ddb6b80f
CynthiaPM <cynthiaeugeniapm@gmail.com>
2023-01-25 12:56:04 +0100

    ejercicio 4 listo

--- ejercicio2_nivel1.php
+++ ejercicio2_nivel1.php
@@ -1,11 +1,13 @@
 <?php 
 
-function escondrijo($X){
+function escondrijo($X=10){
+    
+       
     for ($i=2; $i <= $X ; $i+=2) { 
         echo $i ."</br>";
     }
 }
 
-echo escondrijo(20);
+echo escondrijo();
 
 ?>
