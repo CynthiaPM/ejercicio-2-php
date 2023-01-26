21152d64394fbe6c44ab885c1444ea6d20fb2608
CynthiaPM <cynthiaeugeniapm@gmail.com>
2023-01-26 18:00:56 +0100

    ejercicio 5 listo

---
+++ ejercicio5_nivel1.php
@@ -0,0 +1,23 @@
+<?php
+
+function gradoEstudiante($nota){
+    $maximoNota = 10;
+
+    $porcentaje = ($nota / $maximoNota) * 100;
+
+    if ($porcentaje >=60) {
+        $grado = "Primera Division";
+    }else if ($porcentaje >= 45 && $porcentcaje <60){
+        $grado = "Segunda División";
+    }else if ($porcentaje >=33 && $porcentaje <45){
+        $grado = "Tercera División";
+    }else {
+        $grado = "Reprobado";
+    }
+
+    return $grado;
+}
+
+echo "El grado correspondiente es: ".gradoEstudiante(3);
+
+?>
